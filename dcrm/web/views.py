1949c266958dcd07ab4d26a1ff9d6b3103e58266
FX Drooklyn <dr.f380@hotmail.com>
2023-11-10 22:01:37 +0100

    register done

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
-    # Check if logged in
+    # Check if logging in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -26,4 +27,24 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    #Check if registering
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        #Check validity
+        if form.is_valid():
+            form.save()
+            #Authenticate and log in
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully created an account")
+            return redirect('home')
+        else:
+            return render(request, 'register.html', {'form':form})
+        
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    
