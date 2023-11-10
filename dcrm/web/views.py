c7c709823f40402e7469081212d0903dc754c44a
FX Drooklyn <dr.f380@hotmail.com>
2023-11-10 13:13:24 +0100

    Navbar / Login basics

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
     return render(request, 'home.html', {})
+
+def login_page(request):
+    return render(request, 'home.html', {})
+
+def logout_page(request):
+    return render(request, 'home.html', {})
+
