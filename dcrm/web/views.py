1a0215ea5d94e342d734926a01e1ab2dbd963fe7
FX Drooklyn <dr.f380@hotmail.com>
2023-11-11 12:53:06 +0100

    record view

--- dcrm/web/views.py
+++ dcrm/web/views.py
@@ -52,3 +52,13 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
     
+def customer_record(request, pk):
+    # Check if logged in
+    if request.user.is_authenticated:
+        # Look up record
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    
+    else:
+        messages.success(request, "You must be logged in to view this page")
+        return redirect('home')
